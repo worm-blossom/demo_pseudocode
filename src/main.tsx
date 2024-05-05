ec2f4ab861b1f36f94df230177a5efede30aa89a
Aljoscha Meyer <mail@aljoscha-meyer.de>
2024-05-05 23:28:51 +0200

    Add most mid-level macros

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import {
   Code,
   Config,
   ConfigPseudocode,
+  ConfigStructuredcode,
   Context,
   Counter,
   Def,
+  Delimited,
+  Delimiters,
+  DocComment,
   Em,
   EscapeHtml,
   Expression,
@@ -216,6 +220,177 @@ const exp = (
           </RefLoc>.
         </P>
       </Hsection>
+    </Hsection>
+    <Hsection
+      n="midlevel"
+      title="Mid-level Macros"
+    >
+      <P>
+        The <Code>macromania_structuredcode</Code>{"  "}
+        package provides mid-level facilities for rendering structured code. You
+        probably will not use it directly unless defining your own language
+        constructs. Still, cannot really hurt to learn what is going on under
+        the hood.
+      </P>
+
+      <P>
+        Use the <Code>Delimiters</Code> macro for matching pairs of delimiters.
+      </P>
+
+      <Pseudocode n="delimiters" lineNumbering>
+        <Loc>
+          <Delimiters delims={["(", ")"]}>
+            <Delimiters delims={["(", ")"]}>
+              <Delimiters delims={["(", ")"]}>
+                <Delimiters delims={["(", ")"]}></Delimiters>
+              </Delimiters>
+              <Delimiters delims={["(", ")"]}>
+                <Delimiters delims={["(", ")"]}></Delimiters>
+              </Delimiters>
+              <Delimiters delims={["(", ")"]}></Delimiters>
+            </Delimiters>
+          </Delimiters>
+        </Loc>
+      </Pseudocode>
+
+      <P>
+        The <Code>Delimited</Code>{" "}
+        macro is the bread-and-butter macro for structured code blocks. It wraps
+        some content expression in delimiters, optionally places separator in
+        between them, and optionally places each of them in its own, indented
+        line.
+      </P>
+
+      <P>
+        Further, the <Code>Delimited</Code>{" "}
+        macro allows specifying different rendering styles for the delimiters,
+        that can be toggled via config options.
+      </P>
+
+      <P>
+        The following examples use the same <Code>Delimited</Code>{" "}
+        invocation, except for toggling the <Code>multiline</Code>{" "}
+        flag and for rendering in the three different pseudocode styles: C-like,
+        Python-like, and Ruby-like.
+      </P>
+
+      <P>Single line, Python-style.</P>
+
+      <Pseudocode n="delimitedSinglelinePython" lineNumbering>
+        <Loc>
+          if true{" "}
+          <Delimited
+            separator=";"
+            c={["{", "}"]}
+            pythonSkip
+            ruby={["then", "endif"]}
+            content={["foo()", "bar()", "baz()"]}
+          />{" "}
+          else foo;
+        </Loc>
+      </Pseudocode>
+
+      <P>Multiple lines, Python-style.</P>
+
+      <Pseudocode n="delimitedMultilinePython" lineNumbering>
+        <Loc>
+          if true{" "}
+          <Delimited
+            multiline
+            separator=";"
+            c={["{", "}"]}
+            pythonSkip
+            ruby={["then", "endif"]}
+            content={["foo()", "bar()", "baz()"]}
+          />{" "}
+          else foo;
+        </Loc>
+      </Pseudocode>
+
+      <Config options={[<ConfigStructuredcode delimiterStyle="c"/>]}>
+        <P>Single line, C-style.</P>
+
+        <Pseudocode n="delimitedSinglelineC" lineNumbering>
+          <Loc>
+            if true{" "}
+            <Delimited
+              separator=";"
+              c={["{", "}"]}
+              pythonSkip
+              ruby={["then", "endif"]}
+              content={["foo()", "bar()", "baz()"]}
+            />{" "}
+            else foo;
+          </Loc>
+        </Pseudocode>
+
+        <P>Multiple lines, C-style.</P>
+
+        <Pseudocode n="delimitedMultilineC" lineNumbering>
+          <Loc>
+            if true{" "}
+            <Delimited
+              multiline
+              separator=";"
+              c={["{", "}"]}
+              pythonSkip
+              ruby={["then", "endif"]}
+              content={["foo()", "bar()", "baz()"]}
+            />{" "}
+            else foo;
+          </Loc>
+        </Pseudocode>
+      </Config>
+
+      <Config options={[<ConfigStructuredcode delimiterStyle="ruby"/>]}>
+        <P>Single line, Ruby-style.</P>
+
+        <Pseudocode n="delimitedSinglelineRuby" lineNumbering>
+          <Loc>
+            if true{" "}
+            <Delimited
+              separator=";"
+              c={["{", "}"]}
+              pythonSkip
+              ruby={["then", "endif"]}
+              content={["foo()", "bar()", "baz()"]}
+            />{" "}
+            else foo;
+          </Loc>
+        </Pseudocode>
+
+        <P>Multiple lines, Ruby-style.</P>
+
+        <Pseudocode n="delimitedMultilineRuby" lineNumbering>
+          <Loc>
+            if true{" "}
+            <Delimited
+              multiline
+              separator=";"
+              c={["{", "}"]}
+              pythonSkip
+              ruby={["then", "endif"]}
+              content={["foo()", "bar()", "baz()"]}
+            />{" "}
+            else foo;
+          </Loc>
+        </Pseudocode>
+      </Config>
+
+      <P>
+        The <Code>DocComment</Code> macro renders a full-line comment.
+      </P>
+
+      <Pseudocode n="docComment" lineNumbering>
+        <DocComment>Bla bla bla <Em>blubb</Em> bla bla.</DocComment>
+          <Loc>
+            let x = 17;
+          </Loc>
+          <DocComment><P>Bla bla bla <Em>blubb</Em> bla bla.</P><P>Plip plop plang!</P></DocComment>
+          <Loc>
+            x = 18;
+          </Loc>
+        </Pseudocode>
     </Hsection>
   </ArticleTemplate>
 );
